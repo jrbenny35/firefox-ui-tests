def57f12dffdefd79a99d562feb542c290fb8252
Henrik Skupin <mail@hskupin.info>
2015-12-16 12:54:13 +0100

    Bug 1232967 - Create Python packages for firefox_ui_harness, firefox_puppeteer, and firefox-ui-tests. r=syd r=maja_zf

--- firefox_ui_harness/firefox_ui_harness/__init__.py
+++ firefox_ui_harness/firefox_ui_harness/__init__.py
@@ -2,5 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import cli_functional
-import cli_update
+from firefox_ui_harness.cli_functional import cli_functional
+from firefox_ui_harness.cli_update import cli_update
+
+
+__version__ = '1.0.0'
