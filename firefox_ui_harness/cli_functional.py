c80e9ad87703abd83bf3baeb4cbd2b20957a4759
Henrik Skupin <mail@hskupin.info>
2015-12-21 23:09:14 +0100

    Fix merge conflicts for cli_functional addition

--- firefox_ui_harness/cli_functional.py
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from firefox_ui_harness.runtests import cli as cli_functional
-
-
-if __name__ == '__main__':
-    cli_functional()
