e56d9e6103680f06d4e76391ee36c73e957bc04a
jrbenny35 <bennyjr169@gmail.com>
2016-02-23 17:54:45 -0500

    Code cleanup

--- firefox_ui_tests/firefox_ui_tests/functional/security/test_safe_browsing_primary_download.py
+++ firefox_ui_tests/firefox_ui_tests/functional/security/test_safe_browsing_primary_download.py
@@ -14,7 +14,7 @@ class TestSafeBrowsingPrimaryDownload(FirefoxTestCase):
         {
             'platform': ['linux', 'win', 'darwin'],
             'files': [
-                #Phishing
+                # Phishing
                 "goog-phish-shavar.cache",
                 "goog-phish-shavar.pset",
                 "goog-phish-shavar.sbstore",
@@ -25,8 +25,7 @@ class TestSafeBrowsingPrimaryDownload(FirefoxTestCase):
                 "goog-badbinurl-shavar.pset",
                 "goog-badbinurl-shavar.sbstore",
 
-
-                #Tracking Protections
+                # Tracking Protections
                 "mozstd-track-digest256.cache",
                 "mozstd-track-digest256.pset",
                 "mozstd-track-digest256.sbstore",
@@ -35,31 +34,30 @@ class TestSafeBrowsingPrimaryDownload(FirefoxTestCase):
                 "mozstd-trackwhite-digest256.sbstore"
                 ]
             },
-            {
-                'platform': ['win'],
-                'files': [
-                    "goog-downloadwhite-digest256.cache",
-                    "goog-downloadwhite-digest256.pset",
-                    "goog-downloadwhite-digest256.sbstore"
-                ]
-             }
-        ]
+        {
+            'platform': ['win'],
+            'files': [
+                "goog-downloadwhite-digest256.cache",
+                "goog-downloadwhite-digest256.pset",
+                "goog-downloadwhite-digest256.sbstore"
+            ]
+        }
+    ]
 
     def setUp(self):
         FirefoxTestCase.setUp(self)
+
+        # Set Browser URL
         self.test_url = 'https://mozqa.com'
 
+        # Set Browser Preferences
         self.prefs.set_pref('browser.safebrowsing.provider.google.lastupdatetime', 1)
         self.prefs.set_pref('browser.safebrowsing.provider.google.nextupdatetime', 1)
         self.prefs.set_pref('browser.safebrowsing.enabled', 'true')
+        self.prefs.set_pref('browser.safebrowsing.malware.enabled', 'true')
 
+        # Set variable to join path
         self.sb_files_path = os.path.join(self.marionette.instance.profile.profile, 'safebrowsing')
-        print(self.sb_files_path)
-
-
-        #for item in data:
-        #   if 'linux' in item['platform']:
-        #        print 'platform is linux: %s' % item['platform']
 
     def test_safe_browsing(self):
         with self.marionette.using_context('content'):
@@ -67,39 +65,24 @@ class TestSafeBrowsingPrimaryDownload(FirefoxTestCase):
 
         if sys.platform.startswith('linux'):
             self.verify_files_existence()
-            print("linux")
         elif sys.platform.startswith('win32'):
-            print('win32')
-            #self.verify_win_files_existence()
+            self.verify_win_files_existence()
 
+    # Test all besides windows specific
     def verify_files_existence(self):
         for data in self.data:
             if 'linux' in data['platform']:
                 for item in data['files']:
-                    print("true") ## Remove
                     self.assertTrue(
                         Wait(self.marionette, timeout=300).until(
                             lambda _: os.path.exists(os.path.join(self.sb_files_path, item))))
 
-
+    # Test windows specific files
     def verify_win_files_existence(self):
-        for file in self.win_file_names:
-            if self.assertTrue(
-                    Wait(self.marionette, timeout=60).until(
-                        lambda _: os.path.exists(os.path.join(self.files_location, file)))):
-                self.verify_tracking_protection_existence()
-
-    def verify_tracking_protection_existence(self):
-        for k, v in self.file_names.items():
-            if k == 'tracking':
-                #print('inside linux')
-                #print(v)
-                for file in v:
+        for data in self.data:
+            if 'win' in data['platform']:
+                for item in data['files']:
                     self.assertTrue(
-                        Wait(self.marionette, timeout=30).until(
-                            lambda _: os.path.exists(os.path.join(self.sb_files_path, file))))
+                        Wait(self.marionette, timeout=300).until(
+                            lambda _: os.path.exists(os.path.join(self.sb_files_path, item))))
 
-        #for file in self.tracking_protection:
-        #    self.assertTrue(
-        #        Wait(self.marionette, timeout=60).until(
-        #            lambda _: os.path.exists(os.path.join(self.files_location, file))))
