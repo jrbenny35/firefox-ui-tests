653c8e274b4d339257a0502d96c85497af1e90a9
jrbenny35 <bennyjr169@gmail.com>
2016-02-29 16:34:49 -0500

    Updating branch with changes

--- firefox_ui_tests/firefox_ui_tests/functional/security/test_safe_browsing_primary_download.py
+++ firefox_ui_tests/firefox_ui_tests/functional/security/test_safe_browsing_primary_download.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from marionette_driver import Wait
 from firefox_puppeteer.testcases import FirefoxTestCase
@@ -57,20 +56,24 @@ class TestSafeBrowsingPrimaryDownload(FirefoxTestCase):
     def setUp(self):
         FirefoxTestCase.setUp(self)
 
-        # Restart Browser
-        self.restart()
-
         # Set Browser Preferences
         for item, value in self.browser_prefs.items():
             self.prefs.set_pref(item, value)
+
+        # Restart Browser
+        self.restart()
+
+        for item in self.browser_prefs.items():
+            print self.prefs.get_pref(item)
 
         # Set Variable to tmp safebrowsing directory
         self.sb_files_path = os.path.join(self.marionette.instance.profile.profile, 'safebrowsing')
 
     def test_safe_browsing_initial_download(self):
-        print(self.platform)
         for data in self.test_data:
             if self.platform in data['platforms']:
                 for item in data['files']:
+                    print('true')
                     Wait(self.marionette, timeout=self.browser.timeout_page_load).until(
-                        lambda _: os.path.exists(os.path.join(self.sb_files_path, item)))
+                        lambda _: os.path.exists(os.path.join(self.sb_files_path, item)),
+                        message='Safe Browsing Files not found!')
